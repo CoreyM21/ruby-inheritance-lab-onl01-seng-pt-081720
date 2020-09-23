ce0620e817ed9c74c2ffb5e6f38f2300f2e84375
Corey Martin <coreyjmartin2@gmail.com>
2020-09-23 00:07:08 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -4,9 +4,9 @@ class User
   
   
   
-  def initialize
-    @first_name = first_name
-    @last_name = last_name
- 
+  # def initialize
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
   
-end end
+end
